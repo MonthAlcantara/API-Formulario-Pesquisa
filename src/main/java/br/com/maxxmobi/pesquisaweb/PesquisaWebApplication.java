08cc1f95ef420989b269e3423eac00c5542aa82e
montival Junior <montival_junior@yahoo.com.br>
2020-10-16 09:17:01 -0300

    commit inicial

---
+++ src/main/java/br/com/maxxmobi/pesquisaweb/PesquisaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.maxxmobi.pesquisaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maxxmobi/pesquisaweb/PesquisaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxxmobi.pesquisaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
